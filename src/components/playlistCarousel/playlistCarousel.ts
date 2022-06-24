a98553c5a6b7fc04719ed92f78989c7fff91f881
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2022-06-24 20:35:16 +1000

    add: carousel styling

--- src/components/playlistCarousel/playlistCarousel.ts
+++ src/components/playlistCarousel/playlistCarousel.ts
@@ -9,11 +9,16 @@ export function playlistCarousel(items: Playlist[]) {
     const leftSwitcher = switcher({ direction: 'left', onClick: turnLeft });
     const rightSwitcher = switcher({ direction: 'right', onClick: turnRight });
 
+    // стили добавляем здесь, потому что сам switcher не должен ничего знать о своем положении в контейнере. Это обеспечит переиспользуемость
+    leftSwitcher.classList.add('carousel__control', 'carousel__control--left');
+    rightSwitcher.classList.add('carousel__control', 'carousel__control--right');
     // эквивалент document.createElement('ul');
-    const carouselElement = $('<ul>').addClass('carousel-list')
-                                     .append(playlists)
-                                     .append(leftSwitcher)
-                                     .append(rightSwitcher);
+    const carouselElement = $('<div>').addClass('carousel')
+                                      .append($('<ul>').addClass('carousel-list')
+                                                       .append(playlists))
+                                      .append(leftSwitcher)
+                                      .append(rightSwitcher);
+
 
     function turnLeft() {
 
